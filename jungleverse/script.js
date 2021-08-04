b7418d73c007d29b0c1a6ec1903b69c7d7f7e5ce
Candice Canoso <mail@cndce.me>
2021-08-04 20:26:11 +0800

    [jungleverse] Timeline effect

--- jungleverse/script.js
+++ jungleverse/script.js
@@ -3,6 +3,9 @@
     const domHeader = document.getElementById('cndce-header-wrapper');
 
 
+    const domTimelineItems = document.getElementsByClassName('cndce-timeline-item');
+
+
     function initHeaderSticky(){
         if(window.scrollY > 0){
             domHeader.classList.remove('top');
@@ -11,8 +14,21 @@
         }
     }
 
+    function scrollTimelineDots(){
+        Array.from(domTimelineItems).forEach(function(el){
+            const pos = el.getBoundingClientRect();
+
+            if(pos.top + pos.height / 2 - window.innerHeight / 1.5 <= 0){
+                el.classList.add('active');
+            }else{
+                el.classList.remove('active');
+            }
+        })
+    }
+
     window.addEventListener('scroll', function(){
         initHeaderSticky();
+        scrollTimelineDots();
     })
 
 
