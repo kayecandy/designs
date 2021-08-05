0719a96d699b4e047f75a9dc3e635c1ae9e979ce
Candice Canoso <mail@cndce.me>
2021-08-06 02:03:50 +0800

    [jungleverse] - Mobile responsiveness

--- jungleverse/script.js
+++ jungleverse/script.js
@@ -3,7 +3,11 @@
     const domHeader = document.getElementById('cndce-header-wrapper');
 
 
+    const domRoadmapSection = document.getElementById('roadmap');
     const domTimelineItems = document.getElementsByClassName('cndce-timeline-item');
+
+    const domMobileMenu = document.getElementById('cndce-mobile-menu');
+    const domMobileMenuClose = document.getElementById('cndce-menu-close');
 
 
     function initHeaderSticky(){
@@ -26,10 +30,34 @@
         })
     }
 
+    function showTimelineBg(){
+
+        const pos = domRoadmapSection.getBoundingClientRect
+        ();
+
+        if(pos.top - window.innerHeight <= 0 && pos.bottom >= 0){
+            document.body.classList.add('roadmap');
+        }else{
+            document.body.classList.remove('roadmap');
+        }
+        
+    }
+
     window.addEventListener('scroll', function(){
+        showTimelineBg();
+
         initHeaderSticky();
         scrollTimelineDots();
     })
+
+    domMobileMenu.addEventListener('click', function(){
+        document.body.classList.add('show-menu');
+    })
+
+    domMobileMenuClose.addEventListener('click', function(){
+        document.body.classList.remove('show-menu');
+    })
+
 
 
     initHeaderSticky();
